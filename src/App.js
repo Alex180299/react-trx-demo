55c70588d3318dc2b8f3241889571461b6a37d4a
AlejandroOrtiz <aortiz@kssoluciones.com>
2021-01-13 14:58:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Client } from '@stomp/stompjs';
+import { Component } from 'react';
+import { RadialChart } from 'react-vis';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sessions: [],
+  };
+
+  componentDidMount() {
+    this.client = new Client();
+
+    this.client.configure({
+      brokerURL: 'ws://localhost:8080/socket',
+      onConnect: () => {
+        console.log('onConnect');
+
+        this.client.subscribe('/sessions/config', (message) => {
+          const sessions = JSON.parse(message.body);
+          const chartSessions = sessions.map((session) => {
+            const obj = [];
+            obj.push({
+              angle: session.approved,
+              label: 'Aprobadas',
+            });
+
+            obj.push({
+              angle: session.disapproved,
+              label: 'Rechazadas',
+            });
+
+            return obj;
+          });
+
+          this.setState({ sessions: chartSessions });
+        });
+      },
+    });
+
+    this.client.activate();
+  }
+
+  render() {
+    console.log('Render')
+    const elements = this.state.sessions;
+
+    return (
+      <ul>
+        {elements.map((value, index) => {
+          return <RadialChart data={value} width={300} height={300} showLabels={true} animation={true} />;
+        })}
+      </ul>
+    );
+  }
 }
 
 export default App;
